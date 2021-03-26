e4f1eaf3d8c9917f24005932787facf00c18ba61
topii <topi.ilmoni@gmail.com>
2021-03-26 11:48:41 +0200

    Added 10.1 searchbox and some 10.3 features.

--- app/src/main/java/com/example/viikko10/MainActivity.java
+++ app/src/main/java/com/example/viikko10/MainActivity.java
@@ -5,21 +5,44 @@ import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etSearchBox;
     WebView web;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etSearchBox = findViewById(R.id.etSearchBox);
         web = findViewById(R.id.webView);
         web.setWebViewClient(new WebViewClient());
         web.getSettings().setJavaScriptEnabled(true);
         web.loadUrl("https://www.google.com/");
+        //https://www.google.com/
+        //file:///android_asset/index.html
     }
 
+    public void goButton(View view) {
+        String url = etSearchBox.getText().toString();
+        if (url.isEmpty()) {
+            return;
+        } else if ((url.contains("https://")) || (url.contains("http://"))) {
+            web.loadUrl(url);
+        } else {
+            web.loadUrl("http://" + url);
+        }
+    }
 
     public void refreshButton(View view) {
         web.loadUrl(web.getUrl());
+    }
+
+    public void shoutOutButton(View view) {
+        web.evaluateJavascript("javascript:shoutOut()", null);
+    }
+
+    public void initializeButton(View view) {
+        web.evaluateJavascript("javascript:initialize()", null);
     }
 }
